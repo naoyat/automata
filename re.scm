34bf8b4f8dc658c31df27c8dd6f2885c93def7b2
naoya_t <naoya.tozuka@gmail.com>
2012-05-29 20:02:14 +0900

    ユーティリティ関数の分離

--- re.scm
+++ re.scm
@@ -1,34 +1,6 @@
 (use srfi-1) ;; cons* -> Gauche の組み込み list* でも可
 (use gauche.array)
-
-;; 与えられたリスト ls の要素の重複を排除して返す
-(define (uniq ls . args)
-  (let* ([hash-type (if (null? args) 'eq? (car args))]
-         [ht (make-hash-table hash-type)])
-    (for-each (cut hash-table-put! ht <> #t) ls)
-    (hash-table-keys ht)))
-
-;; 与えられたリスト ls の全要素を持つハッシュテーブルを作る
-(define (list->set ls . args)
-  (let* ([hash-type (if (null? args) 'eq? (car args))]
-         [ht (make-hash-table hash-type)])
-    (for-each (cut hash-table-put! ht <> #t) ls)
-    ht))
-
-;; ある要素が予め与えられたリストに含まれるか否かを返す関数を作成
-(define (make-set-has?-proc ls . args)
-  (let1 ht (apply list->set ls args)
-    (lambda (elem) (hash-table-get ht elem #f))))
-
-;; 与えられたリスト ls の全要素に連番を振ったハッシュテーブルを作る
-(define (list->idmap ls . args)
-  (let* ([hash-type (if (null? args) 'eq? (car args))]
-         [ht (make-hash-table hash-type)])
-    (let loop ((id 0) (ls ls))
-      (if (null? ls) ht
-          (begin
-            (hash-table-put! ht (car ls) id)
-            (loop (+ id 1) (cdr ls)))))))
+(require "./myutil")
 
 ;; ある要素に与えられた連番を返す。未知の要素に対しては #f を返す。
 (define (make-objectid-proc ls . args)
